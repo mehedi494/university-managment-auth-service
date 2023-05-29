9fbee13c6f1c4512335f0bc14b310ea797daf521
mehedi494 <ehasanmunna21@gmail.com>
2023-05-29 19:37:09 +0600

    fixed export import err

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application,Request,Response, json} from "express";
+import express, { Application,Request,Response} from "express";
 import cors from "cors"
 
 const app: Application = express()
